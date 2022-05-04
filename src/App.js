1c3c72c2b7bde5ae6c82122cc73690af8e65e08a
Sam Shock <sam.shock924@gmail.com>
2022-05-03 20:13:28 -0400

    random updates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import ExpenseForm from './ExpenseForm';
 
 function App() {
+  const title = 'Expense Tracker';
+  const handleClick = () => {
+    console.log('click works');
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app-title">
+        <h1>{title}</h1>
+      </div>
+      <div className="container">
+        <button className="create-expense-button" onClick={handleClick}>Create New Expense</button>
+      </div>
     </div>
   );
 }
---
+++ src/ExpenseForm.js
@@ -0,0 +1,8 @@
+export default function ExpenseForm() {
+    
+    return ( 
+        <div className="ExpenseForm">
+            
+        </div>
+     );
+}
