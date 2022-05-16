03b5b2b2f0e37c6338aca6c71f024ef50ce18a63
Sam Shock <sam.shock924@gmail.com>
2022-05-15 21:34:53 -0400

    basic table made

--- react-expense-tracker/src/ExpenseTable.js
+++ react-expense-tracker/src/ExpenseTable.js
@@ -1,7 +1,20 @@
+import Table from 'react-bootstrap/Table';
+
 const ExpenseTable = () => {
     return ( 
         <div className="expense-table">
-            <h1>This is the expense table</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Description</th>
+                        <th>Payment Type</th>
+                        <th>Amount</th>
+                    </tr>
+                </thead>
+                <tbody className="">
+                </tbody>
+            </table>
         </div>
      );
 }
